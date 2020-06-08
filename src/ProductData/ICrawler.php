3379d1d8f5576e85aa79794123b3b6afd50af69e
Alexey Melnikov <melnikoff@gmail.com>
2020-06-08 14:37:43 +0200

    Still trying to make it compatible with recent Nette versions

--- src/ProductData/ICrawler.php
+++ src/ProductData/ICrawler.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace ProductData;
 
 use Nette\Http\Url;
+use \Dibi\Row;
 
 interface ICrawler
 {
-	/**
-	 * @param Url $url
-	 * @return array|null
-	 */
-	public function debug(Url $url);
+    /**
+     * @param Url $url
+     * @return array|null
+     */
+    public function debug(Url $url);
 
-	/**
-	 * @return void
-	 */
-	public function start();
+    /**
+     * @return void
+     */
+    public function start(Row $importRow);
 }
